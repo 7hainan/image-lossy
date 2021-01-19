d1e23135a214084b706f185f60e079883c33c6a5
Thainan <thainan239@gmail.com>
2021-01-18 23:34:47 -0300

    starting to configure layout

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Lossy</title>
+
+    <style>
+        body {
+            background-color: #161b22;
+            color: #f0f6fc;
+            margin:0;
+            padding:0;
+        }
+        .main-view{
+            display:flex;
+            justify-content:center;
+            flex-direction:row;
+            /* border:solid 1px #CCC; */
+            /* width:100%; */
+            height:400px;
+            align-self:center;
+            padding:4px 4px 4px 4px;
+        }
+        .main-form-input{
+            width:80%;
+            height:100px;
+            background-color:#000;
+            margin-top:05%;
+        }
+    </style>
+
+</head>
+
+<body>
+   <div class="main-view">
+        <div class="main-form-input">
+            <form action="#">
+                <input type="file" class="imput-field">
+            </form>
+           
+        </div>
+   </div>
+</body>
+
+</html>
